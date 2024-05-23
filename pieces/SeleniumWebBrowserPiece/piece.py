ba86b0ed6011f3e0455cb323d0b30f3ae7269f62
Nathan Vieira Marcelino <nathan.v.marcelino@gmail.com>
2024-05-23 19:24:41 -0300

    try fix selenium options

--- pieces/SeleniumWebBrowserPiece/piece.py
+++ pieces/SeleniumWebBrowserPiece/piece.py
@@ -1,24 +1,32 @@
 from domino.base_piece import BasePiece
 from .models import InputModel, OutputModel, CommandOutput
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 class SeleniumWebBrowserPiece(BasePiece):
 
     def piece_function(self, input_data: InputModel):
+        self.logger.info("Create chrome options arguments.")
 
-        self.logger.info("Execution started")
-        options = webdriver.ChromeOptions()
+        options = Options()
         options.add_argument("--headless=new")
+        options.add_argument("--no-sandbox")
 
+        self.logger.info("Start chrome web driver.")
         driver = webdriver.Chrome(options=options)
 
+        self.logger.info("Going to URL.")
         driver.get("https://webscraper.io/test-sites/e-commerce/static")
 
-        self.logger.info("Maximizing window")
+        self.logger.info("Maximize window.")
         driver.maximize_window()
 
+        self.logger.info("Taking screenshot.")
+        driver.save_screenshot('ss.png')
+
+        self.logger.info("Quit chrome driver.")
         driver.quit()
 
         return OutputModel(
-            outputs=[CommandOutput(name="out1", result="res1")]
+            outputs=[dict(name="out1", result="res1")]
         )
